5c3f15f8707a20d0576f727b1d4458b820202ff4
Matt Farmer <matt@frmr.me>
2015-12-05 01:11:41 -0500

    Bump version to 0.2.0-SNAPSHOT for forward development.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dstt "0.1.0"
+(defproject dstt "0.2.0-SNAPSHOT"
   :description "The Damn Simple Test Tool"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [clj-http "2.0.0"]
