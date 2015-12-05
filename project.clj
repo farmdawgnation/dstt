03b8129a00c54986391ea39f71c9d17717e66328
Matt Farmer <matt@frmr.me>
2015-12-05 15:27:41 -0500

    Fill in the missing important bits in project file.

--- project.clj
+++ project.clj
@@ -1,7 +1,11 @@
 (defproject dstt "0.2.0-SNAPSHOT"
   :description "The Damn Simple Test Tool"
+  :url "https://github.com/farmdawgnation/dstt"
+  :license {:name "Apache License 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [clj-http "2.0.0"]
                  [org.clojure/tools.cli "0.3.3"]
                  [org.clojure/data.json "0.2.6"]]
-  :main frmr.dstt)
+  :main frmr.dstt
+  :aot [frmr.dstt])
