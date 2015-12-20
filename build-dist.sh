9f2922c147225bfa93aa8946bcf8f732e76a5377
Matt Farmer <matt@frmr.me>
2015-12-20 10:33:12 -0500

    Add distribution builder script.

---
+++ build-dist.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -e
+
+VERSION=$(cat project.clj | head -n 1 | awk '{print substr($3, 2, length($3)-2)}')
+JAR_NAME="target/dstt-$VERSION-standalone.jar"
+
+echo "Building distribution for $VERSION"
+
+echo "Building uberjar..."
+lein uberjar
+
+echo "Writing invocation script..."
+mkdir target/dstt-$VERSION
+echo "#!/bin/bash" > target/dstt-$VERSION/dstt
+echo "" >> target/dstt-$VERSION/dstt
+echo "java -jar dstt-$VERSION-standalone.jar \"$@\"" >> target/dstt-$VERSION/dstt
+chmod ugo+x target/dstt-$VERSION/dstt
+
+echo "Copying over files..."
+cp README.md target/dstt-$VERSION/
+cp $JAR_NAME target/dstt-$VERSION/
+
+echo "Building xz archive..."
+tar cJf target/dstt-$VERSION.tar.xz target/dstt-$VERSION
+mv target/dstt-$VERSION.tar.xz .
+
+echo "Distribution built."
