9f8e36a66ccb8e4164499725bc457b9dec497645
Matt Farmer <matt@frmr.me>
2015-12-22 16:54:36 -0500

    Bump version to 0.3.0 for release.
    [ci skip]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject me.frmr.tools/dstt "0.3.0-SNAPSHOT"
+(defproject me.frmr.tools/dstt "0.3.0"
   :description "Damn Simple Test Tool, a simple and rudamentary load testing library."
   :url "https://github.com/farmdawgnation/dstt"
   :license {:name "Apache License 2.0"
